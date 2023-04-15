a6f7a4c5b21d1f567cfbd11384e6fc5643f83cda
Chemi90 <116808913+Chemi90@users.noreply.github.com>
2023-04-15 14:09:36 +0200

    Delete Clases1DAM directory

--- Clases1DAM/src/tema9Caso2/ExcepcionDiaSup100.java
+++
@@ -1,7 +0,0 @@
-package tema9Caso2;
-
-public class ExcepcionDiaSup100 extends Exception{
-	public ExcepcionDiaSup100 () {
-		super("El diametro supera los 100km.");
-	}
-}
--- Clases1DAM/src/tema9Caso2/ExcepcionGeneral.java
+++
@@ -1,7 +0,0 @@
-package tema9Caso2;
-
-public class ExcepcionGeneral extends Exception{
-	public ExcepcionGeneral () {
-		super("HA OCURRIDO UNA EXCEPCIÓN.");
-	}
-}
